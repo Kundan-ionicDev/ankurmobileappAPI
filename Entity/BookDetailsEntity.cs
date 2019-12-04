e03b50be17ee8cb03513fbe8fae6dec48f390ca4
Kundan Sakpal <kundan.mobileappdev@gmail.com>
2019-12-04 19:49:20 +0530

    Merged Conflicts

--- Entity/BookDetailsEntity.cs
+++ Entity/BookDetailsEntity.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -53,60 +52,4 @@ namespace AnkurPrathisthan.Entity
         public string Message { get; set; }
     }
 
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace AnkurPrathisthan.Entity
-{
-    public class BookDetailsEntity
-    {
-        public string BookName { get; set; }      
-        public string PublisherName  { get; set; }   
-        public string AuthorName  { get; set; }   
-        public string Price  { get; set; }
-        public string Stock { get; set; }
-        public string CategoryName  { get; set; }   
-        public string Language  { get; set; }   
-        public string EmailID { get; set; }
-        public string cmd { get; set; }
-        public string LanguageID { get; set; }
-        public string PublisherID { get; set; }
-        public string CategoryID { get; set; }
-        public string BookID { get; set; }
-        public string Message { get; set; }
-
-    }   
-
-    public class CategoryDetails
-    {
-        public string CategoryID { get; set; }
-        public string CategoryName { get; set; }
-        public string CreatedBy { get; set; }
-        public string Message { get; set; }
-        public string ModifiedBy { get; set; }
-
-    }
-
-    public class LanguageDetails
-    {
-        public string LanguageID { get; set; }
-        public string LanguageName { get; set; }
-        public string CreatedBy { get; set; }
-        public string Message { get; set; }
-        public string ModifiedBy { get; set; }  
-    }
-
-    public class PublisherDetails
-    {
-        public string PublisherID { get; set; }
-        public string PublisherName { get; set; }
-        public string CreatedBy { get; set; }
-        public string ModifiedBy { get; set; }
-        public string Message { get; set; }
-    }
-
->>>>>>> 70e1adf51826a94637acb0e5e859d47eef8abb3d
 }
--- Entity/ClusterDetailsEntity.cs
+++ Entity/ClusterDetailsEntity.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,28 +20,4 @@ namespace AnkurPrathisthan.Entity
         public string ModifiedBy { get; set; }
         public string Message { get; set; }
     }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace AnkurPrathisthan.Entity
-{
-    public class ClusterDetailsEntity
-    {
-        public string ClusterName { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }        
-        public string ClusterCode { get; set; }
-        public string MobileNo { get; set; }       
-        public string Librarian { get; set; }
-        public string Members { get; set; }
-        public string CreatedBy { get; set; }
-        public string CreatedDate { get; set; }
-        public string ModifiedDate { get; set; }
-        public string ModifiedBy { get; set; }
-        public string Message { get; set; }
-    }
->>>>>>> 70e1adf51826a94637acb0e5e859d47eef8abb3d
 }
